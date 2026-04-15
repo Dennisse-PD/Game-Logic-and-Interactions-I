3290baf86558be580f06936747d9d03380b144c4
Dennisse-PD <dennisse.freelancedevelopments@gmail.com>
2026-04-15 17:06:29 -0400

    Implemented enemy patrol AI with randomized positions

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -1,37 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem; 
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
 
 public class AI : MonoBehaviour
 {
     // Store all points, object array?
-    [SerializeField] private Transform[] _points;
+   [SerializeField] private Transform[] _points;
 
     // Select a random waypoint, .random on the array?
-    private int _destinationPoint = Random.Range(0, _points.Length);
+    private int _destinationPoint;
 
     // Traverse to the random point, how do we do this in nav mesh? Check nav agen documentation 
-    private NavMeshAgent _agent;
+    [SerializeField] private NavMeshAgent _agent;
 
     // Start is called before the first frame update
     void Start()
     {
         //check if nav mesh agent is available to avoid runtime errors
-        if( _agent == null )
+        if (_agent == null)
         {
-            Error.Log("Nav mesh agent is null!");
+            Debug.LogError("Nav mesh agent is null!");
         }
         //Look for our nav mesh
-        _agent = GetComponent<NavMeshAgent>();
+        //_agent = GetComponent<NavMeshAgent>();
 
         //Disables auto-breaking for continous movement between points
         _agent.autoBraking = false;
+
+        //assingin values
+        //_destinationPoint = Random.Range(0, _points.Length);
+
+        //call function to move to next random point
+        GotoNextPoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
+            GotoNextPoint();
     }
+    private void GotoNextPoint()
+    {
+        // Returns if no points have been set up, in other words, function won't run
+        if (_points.Length == 0)
+        {
+            Debug.Log("Point lengh is = to 0 ");
+            //return;
+        }
+               
+
+        _destinationPoint = Random.Range(0, _points.Length);
+
+        //set the agent to go to the next position, randomied by assigning it before ^
+      _agent.destination =  _points[_destinationPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        _destinationPoint = (_destinationPoint + 1) % Random.Range(0, _points.Length);
+    }
+
 }
